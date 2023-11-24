dde4dad43793f9c84c18c409e9c13afa6c1a5881
narutooxox <112558957+narutooxox@users.noreply.github.com>
2023-11-24 14:32:37 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -15,11 +15,11 @@ async def channel_post(client: Client, message: Message):
     reply_text = await message.reply_text("Please Wait...!", quote = True)
     try:
         post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
-        post_message_1 = await message.forward(chat_id=-1002035056533, disable_notification=True)
+        post_message1 = await message.copy(chat_id=-1002035056533, disable_notification=True)
     except FloodWait as e:
         await asyncio.sleep(e.x)
-        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
-        post_message_1 = await message.forward(chat_id=-1002035056533, disable_notification=True)
+        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
+        post_message1 = await message.copy(chat_id=-1002035056533, disable_notification=True)
     except Exception as e:
         print(e)
         await reply_text.edit_text("Something went Wrong..!")
