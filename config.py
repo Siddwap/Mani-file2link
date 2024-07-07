e61bff22599a750f1f00ba1dc90063e5f7cfec3c
narutooxox <uzumakinarutorasengan0x0@gmail.com>
2024-07-07 13:42:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://karthickjk:karthick@clust
 DB_NAME = os.environ.get("DATABASE_NAME", "manii")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002099714997"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002177119731"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
